4d8ea209acec327f1238793f40c4ede039194de2
Julia Kilarska <julia.kilarska@icloud.com>
2023-02-27 16:27:53 +0100

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hi, my name is Julia");
+        System.out.println("I am 21 years old");
+        System.out.println("K");
+    }
+}
