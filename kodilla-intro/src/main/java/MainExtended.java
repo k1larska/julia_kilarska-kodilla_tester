dafbfb324893b7af9cbca38719ef09f9972451d0
Julia Kilarska <julia.kilarska@icloud.com>
2023-02-28 21:31:44 +0100

    zmienne-zadanie

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Cristiano Ronaldo";
+        String clubName = "Al-Nassr";
+        String position = "forward";
+        int birthYear = 1985;
+        double goals = 709;
+        boolean worldCup = false;
+        System.out.println(name);
+        System.out.println(clubName);
+        System.out.println(position);
+        System.out.println(birthYear);
+        System.out.println(goals);
+        System.out.println(worldCup);
+    }
+}
