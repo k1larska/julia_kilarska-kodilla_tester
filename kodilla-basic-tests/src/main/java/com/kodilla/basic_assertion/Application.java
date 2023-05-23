6ff19e765cda15768ed3ff9dc57f21dec47d9ed7
Julia Kilarska <julia.kilarska@icloud.com>
2023-05-23 19:55:08 +0200

    rozbudowa testów

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,30 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+        int subResult = calculator.subtract(a, b);
+        boolean correct1 = ResultChecker.assertEquals(-3, subResult);
+        if (correct1) {
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+        int sqrResult = calculator.square(a);
+        boolean correct2 = ResultChecker.assertEquals(25, sqrResult);
+        if (correct1) {
+            System.out.println("Metoda square działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda square nie działa poprawnie dla liczby " + a);
+        }
+    }
+}
